1bee23a348ec378323e9badff02879d7f456c3c8
196PC <thai.nguyentien@meditech.vn>
2020-12-03 09:59:58 +0000

    removed buildir

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/my_samples/sample_onnx
-# Build directory: /workspace/my_samples/sample_onnx/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
